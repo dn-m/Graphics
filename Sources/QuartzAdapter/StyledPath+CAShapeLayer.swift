ddc2db7d2fb6b08bb62c86601f02e904f3a81491
James <jbean46@gmail.com>
2018-08-01 13:24:02 -0700

    Comment out offensive bits for now

--- Sources/QuartzAdapter/StyledPath+CAShapeLayer.swift
+++ Sources/QuartzAdapter/StyledPath+CAShapeLayer.swift
@@ -18,11 +18,11 @@ extension CAShapeLayer {
         let styling = renderedPath.styling
         fillColor = styling.fill.color.cgColor
 
-        if #available(macOS 10.14, *) {
-            fillRule = styling.fill.rule.cgFillRule
-            lineCap = styling.stroke.cap.cgCap
-            lineJoin = styling.stroke.join.cgJoin
-        }
+//        if #available(macOS 10.14, *) {
+//            fillRule = styling.fill.rule.cgFillRule
+//            lineCap = styling.stroke.cap.cgCap
+//            lineJoin = styling.stroke.join.cgJoin
+//        }
 
         strokeColor = styling.stroke.color.cgColor
         lineDashPattern = styling.stroke.dashes?.pattern.map { NSNumber.init(value: $0) }
@@ -38,45 +38,45 @@ extension CAShapeLayer {
     }
 }
 
-internal extension Fill.Rule {
-
-    @available(macOS 10.14, *)
-    var cgFillRule: CAShapeLayerFillRule {
-        switch self {
-        case .nonZero:
-            return .nonZero
-        case .evenOdd:
-            return .evenOdd
-        }
-    }
-}
-
-internal extension Stroke.Cap {
-
-    @available(macOS 10.14, *)
-    var cgCap: CAShapeLayerLineCap {
-        switch self {
-        case .butt:
-            return .butt
-        case .round:
-            return .round
-        case .square:
-            return .square
-        }
-    }
-}
-
-internal extension Stroke.Join {
-
-    @available(macOS 10.14, *)
-    var cgJoin: CAShapeLayerLineJoin {
-        switch self {
-        case .miter:
-            return .miter
-        case .bevel:
-            return .bevel
-        case .round:
-            return .round
-        }
-    }
-}
+//internal extension Fill.Rule {
+//
+//    @available(macOS 10.14, *)
+//    var cgFillRule: CAShapeLayerFillRule {
+//        switch self {
+//        case .nonZero:
+//            return .nonZero
+//        case .evenOdd:
+//            return .evenOdd
+//        }
+//    }
+//}
+//
+//internal extension Stroke.Cap {
+//
+//    @available(macOS 10.14, *)
+//    var cgCap: CAShapeLayerLineCap {
+//        switch self {
+//        case .butt:
+//            return .butt
+//        case .round:
+//            return .round
+//        case .square:
+//            return .square
+//        }
+//    }
+//}
+//
+//internal extension Stroke.Join {
+//
+//    @available(macOS 10.14, *)
+//    var cgJoin: CAShapeLayerLineJoin {
+//        switch self {
+//        case .miter:
+//            return .miter
+//        case .bevel:
+//            return .bevel
+//        case .round:
+//            return .round
+//        }
+//    }
+//}
