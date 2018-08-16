cb86b962c70e6479e20ac7818b467a5847573376
James <jbean46@gmail.com>
2018-08-16 15:20:45 -0400

    Remove dead tests, refine current

--- Tests/PathTests/QuadraticBezierCurveTests.swift
+++ Tests/PathTests/QuadraticBezierCurveTests.swift
@@ -14,83 +14,31 @@ class QuadraticBezierCurveTests: XCTestCase {
     
     // MARK: - Quadratic
     
-//    func testTAtMinX() {
-//        
-//        let slopeDown = BezierCurve(
-//            start: Point(x: 0, y: 1),
-//            control: Point(x: 0, y: 0),
-//            end: Point(x: 1, y: 0)
-//        )
-//        
-//        XCTAssertEqual(slopeDown.t(at: (.min, .horizontal)), 0)
-//    }
-//    
-//    func testTAtMaxX() {
-//        
-//        let slopeDown = BezierCurve(
-//            start: Point(x: 0, y: 1),
-//            control: Point(x: 0, y: 0),
-//            end: Point(x: 1, y: 0)
-//        )
-//        
-//        XCTAssertEqual(slopeDown.t(at: (.max, .horizontal)), 1)
-//    }
-//    
-//    func testTAtMinY() {
-//        
-//        let slopeDown = BezierCurve(
-//            start: Point(x: 0, y: 1),
-//            control: Point(x: 0, y: 0),
-//            end: Point(x: 1, y: 0)
-//        )
-//        
-//        XCTAssertEqual(slopeDown.t(at: (.min, .vertical)), 1)
-//    }
-//    
-//    func testTAtMaxY() {
-//        
-//        let slopeDown = BezierCurve(
-//            start: Point(x: 0, y: 1),
-//            control: Point(x: 0, y: 0),
-//            end: Point(x: 1, y: 0)
-//        )
-//        
-//        XCTAssertEqual(slopeDown.t(at: (.max, .vertical)), 0)
-//    }
-    
     func testYsAtX() {
-
         let slopeDown = BezierCurve(
             start: Point(x: 0, y: 1),
             control: Point(x: 0, y: 0),
             end: Point(x: 1, y: 0)
         )
-        
         stride(from: Double(0), to: 1, by: 0.01).forEach { t in
-            
             let point = slopeDown[t]
             let ys = slopeDown.ys(x: point.x)
-            
             XCTAssertEqual(ys.count, 1)
-            XCTAssertEqual(ys.first!, point.y, accuracy: 0.0000001)
+            XCTAssertEqual(ys.first!, point.y, accuracy: 1e-6)
         }
     }
     
     func testXsAtY() {
-        
         let slopeDown = BezierCurve(
             start: Point(x: 0, y: 1),
             control: Point(x: 0, y: 0),
             end: Point(x: 1, y: 0)
         )
-        
         stride(from: Double(0), to: 1, by: 0.01).forEach { t in
-            
             let point = slopeDown[t]
             let xs = slopeDown.xs(y: point.y)
-            
             XCTAssertEqual(xs.count, 1)
-            XCTAssertEqual(xs.first!, point.x, accuracy: 0.0000001)
+            XCTAssertEqual(xs.first!, point.x, accuracy: 1e-6)
         }
     }
 }
